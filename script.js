c0caa500871a24c6fa5a4de3b24874fcb0700e5b
Saimun Hassan <saimunhas@gmail.com>
2023-08-12 16:52:02 -0400

    Added error message to display if error occurs in fetching data

--- script.js
+++ script.js
@@ -14,17 +14,24 @@ const options = {
 const apiURL = 'https://api.api-ninjas.com/v1/chucknorris?';
 
 async function getJoke() {
-  joke.textContent = 'Updating...';
-  btn.disabled = true;
-  btn.innerText = 'Loading...';
-  const response = await fetch(apiURL, options);
-  const data = await response.json();
+  try {
+    joke.textContent = 'Updating...';
+    btn.disabled = true;
+    btn.innerText = 'Loading...';
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
 
-  btn.disabled = false;
-  btn.innerText = 'Tell me a joke';
+    btn.disabled = false;
+    btn.innerText = 'Tell me a joke';
 
-  console.log(data.joke);
-  joke.textContent = data.joke;
+    console.log(data.joke);
+    joke.textContent = data.joke;
+  } catch (error) {
+    joke.style.color = 'red';
+    joke.textContent = 'Ughh.. the site has hit a snag. Try again';
+    btn.disabled = false;
+    btn.innerText = 'Tell me a joke';
+  }
 }
 
 btn.addEventListener('click', getJoke);
